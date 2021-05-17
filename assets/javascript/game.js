e61040fb0361991df0be4f1ca8bb774b199cdd57
Debra Mella <debra.mella@libertymutual.com>
2021-05-17 08:20:48 -0500

    Updating with correct file names

---
+++ assets/javascript/game.js
@@ -0,0 +1,27 @@
+let team1ShootBtn = document.querySelector("#teamone-shoot-button");
+let team2ShootBtn = document.querySelector("#teamtwo-shoot-button");
+
+
+
+
+
+// when the button is clicked the counter goes up
+team1ShootBtn.addEventListener("click", function(){
+  alert("that worked");
+  /* console.log(counter.innerHTML) */
+  /* needed to add parseInt to convert string to a number to add */
+  /* let newCounterValue = parseInt(counter.innerHTML) + 1;*/
+  
+  // update the counter on the screen to have the new value using the innerhtml/innertext for the value
+  //  counter.innerText = newCounterValue;
+  //  console.log(newCounterValue);
+  })
+
+
+// update the counter on the scren to have the new vlue using the innerhtml/innertext for the value
+//if (counter.innerText >0){
+//  counter.innerText = newCounterValue;
+ //   console.log(newCounterValue);
+ // }
+  //proving that the above variables are holding the data I expect
+//  console.log(incrementButton, decrementButton, counter);
