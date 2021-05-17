eb416c14b008e1b119956f4ae2a54fa795ab5d3f
Debra Mella <debra.mella@libertymutual.com>
2021-05-17 09:31:55 -0500

    Code to have shots taken value increase with button click

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,27 +1,30 @@
 let team1ShootBtn = document.querySelector("#teamone-shoot-button");
 let team2ShootBtn = document.querySelector("#teamtwo-shoot-button");
 
+let team1NumShots = document.querySelector("#teamone-numshots");
+let team2NumShots = document.querySelector("#teamtwo-numshots");
 
 
-
-
-// when the button is clicked the counter goes up
+// when the team 1 button is clicked the counter goes up
 team1ShootBtn.addEventListener("click", function(){
-  alert("that worked");
-  /* console.log(counter.innerHTML) */
+  /*alert("that worked"); */
+
   /* needed to add parseInt to convert string to a number to add */
-  /* let newCounterValue = parseInt(counter.innerHTML) + 1;*/
+  let totalTeam1Shots = parseInt(team1NumShots.innerHTML) + 1;
   
   // update the counter on the screen to have the new value using the innerhtml/innertext for the value
-  //  counter.innerText = newCounterValue;
-  //  console.log(newCounterValue);
+  team1NumShots.innerText = totalTeam1Shots;
+  console.log(totalTeam1Shots);
   })
 
 
-// update the counter on the scren to have the new vlue using the innerhtml/innertext for the value
-//if (counter.innerText >0){
-//  counter.innerText = newCounterValue;
- //   console.log(newCounterValue);
- // }
-  //proving that the above variables are holding the data I expect
-//  console.log(incrementButton, decrementButton, counter);
+// when the team 2 button is clicked the counter goes up
+team2ShootBtn.addEventListener("click", function(){
+
+  /* needed to add parseInt to convert string to a number to add */
+  let totalTeam2Shots = parseInt(team2NumShots.innerHTML) + 1;
+  
+  // update the counter on the screen to have the new value using the innerhtml/innertext for the value
+  team2NumShots.innerText = totalTeam2Shots;
+  console.log(totalTeam2Shots);
+  })
