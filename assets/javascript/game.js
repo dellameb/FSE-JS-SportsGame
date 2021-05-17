6e3ad7d692de2b5dd6c558b267e2abb9a98aa189
Debra Mella <debra.mella@libertymutual.com>
2021-05-17 09:51:49 -0500

    added basic JS for reset button

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,6 +4,20 @@ let team2ShootBtn = document.querySelector("#teamtwo-shoot-button");
 let team1NumShots = document.querySelector("#teamone-numshots");
 let team2NumShots = document.querySelector("#teamtwo-numshots");
 
+let resetBtn = document.querySelector("#reset-button");
+let numResets = document.querySelector("#num-resets");
+
+// when the reset button is clicked shot and goal counters reset and reset button incr + 1
+resetBtn.addEventListener("click", function(){
+  //alert("that worked");
+
+    /* needed to add parseInt to convert string to a number to add */
+    let totalResets = parseInt(numResets.innerHTML) + 1;
+  
+    // update the counter on the screen to have the new value using the innerhtml/innertext for the value
+    numResets.innerText = totalResets;
+    console.log(totalResets);
+})
 
 // when the team 1 button is clicked the counter goes up
 team1ShootBtn.addEventListener("click", function(){
